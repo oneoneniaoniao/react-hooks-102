9ad3d0321d90fdaf2611bb2cb825e9535b4aa250
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2022-06-01 08:07:39 +0900

    implement event creation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React,{useReducer, useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
+import reducer from "../reducers";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const addEvent = e => {
+    e.preventDefault();
+    dispatch({
+      type:"CREATE_EVENT",
+      title:title,
+      body:body
+    });
+    setTitle("");
+    setBody("");
+  }
+
+  const deleteAllEvent = e => {
+    e.preventDefault();
+    dispatch({
+      type:"DELETE_ALL_EVENT",
+
+    })
+  }
+
+  console.log(state);
+
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -10,16 +37,16 @@ const App = () => {
           <label htmlFor="formEventTitle">
             タイトル
           </label>
-          <input className="form-control" id="formEventTitle"/>
+          <input className="form-control" id="formEventTitle" value={title} onChange={e=>setTitle(e.target.value)}/>
         </div>
         <div className="form-group">
           <label htmlFor="formEventBody">
             内容
           </label>
-          <textarea className="form-control" id="formEventBody"/>
+          <textarea className="form-control" id="formEventBody" value={body} onChange={e=>setBody(e.target.value)}/>
         </div>
-        <button className="btn btn-primary">イベントを作成する</button>
-        <button style={{ marginLeft: "5px"}} className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
+        <button onClick={deleteAllEvent} style={{ marginLeft: "5px"}} className="btn btn-danger">全てのイベントを削除する</button>
       </form>
       <h4>イベント一覧</h4>
       <table className="table table-hover">
